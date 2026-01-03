30fcc47524c49402d87cb5ef6141cd258cc75b72
v.burlin <vladislav.b252@rambler.ru>
2026-01-03 22:15:21 +0200

    Добавляем проект и первый шаблон с примером

---
+++ CodeTemplatesProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using CodeTemplatesProject.Templates.BehaviorPatterns;
+
+namespace CodeTemplatesProject;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        // Define template to run
+        var template = new ChainOfResponsibility();
+        template.Run();
+    }
+}
